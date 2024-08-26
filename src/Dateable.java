e607c79844f81234461455c338efdb39869e72c6
oliverchan698 <oliverhohochan@gmail.com>
2024-08-26 09:27:16 +0800

    Lab 1

--- src/Dateable.java
+++ src/Dateable.java
@@ -1,6 +1,12 @@
-public class Dateable {
+// Calculates the minimum age of a partner you can date at your age.
+import java.util.Scanner;
 
+public class Dateable {
     public static void main(String[] args) {
-        
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter your age: ");
+        int age = input.nextInt();
+        System.out.println(age + "-year-olds should date somebody who is at least " + (7+(age/2)) + " years old.");
+
     }
 }
--- src/Pages.java
+++ src/Pages.java
@@ -1,6 +1,11 @@
-public class Pages {
+import java.util.Scanner;
 
+public class Pages {
     public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("How old are you? ");
+        int age = input.nextInt();
+        System.out.println("You should read at least " + (100-age) + " pages before giving up on a book.");
 
     }
 }
